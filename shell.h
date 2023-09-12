3e617110bc5761cbeb7e465bc7c59f73acee29c1
bini34 <biniyamambachew@gmail.com>
2023-09-12 04:54:38 +0300

    update header file

--- shell.h
+++ shell.h
@@ -4,4 +4,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void execmd(char **av, char **envp);
+void parseInput(char *lineptr, char ***av, ssize_t *nchars, int *num_tokens);
 #endif
