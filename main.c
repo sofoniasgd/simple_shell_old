0635beacc348ace7f5361f73ede0ecc26d58b9ab
sofoniasgd <zephaniahsofa@gmail.com>
2023-09-14 19:25:11 +0000

    update main.c

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
  * @argc: The number of command-line arguments.
  * @argv: An array of strings containing the command-line arguments.
  * @envp: An array of strings containing environment variables.
- *
  * Return: Always returns 0.
  */
 int main(int argc, char *argv[], char *envp[])
@@ -16,8 +15,10 @@ int main(int argc, char *argv[], char *envp[])
 	pid_t pid;
 
 	(void)argc;
+	/* start shell */
 	while (1)
 	{
+		/* print prompt, read command from stdin and handle errors */
 		write(STDOUT_FILENO, "#cisfun$ ", 9);
 		nchars_read = getline(&lineptr, &n, stdin);
 		if (nchars_read == -1)
@@ -26,6 +27,7 @@ int main(int argc, char *argv[], char *envp[])
 			free(lineptr);
 			return (-1);
 		}
+		/* tokenize the command line, fork and execute from child process*/
 		parseInput(lineptr, &av, &nchars_read, &num_tokens);
 		pid = fork();
 
@@ -37,6 +39,7 @@ int main(int argc, char *argv[], char *envp[])
 			perror(argv[0]);
 			exit(EXIT_FAILURE);
 		}
+		/* wait for child pr, free memory holding tokens and command */
 		else
 		{
 			if (wait(&status) == -1)
