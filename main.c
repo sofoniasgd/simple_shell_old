de9e4eb6088d681962fe770bf75af11592668402
sofoniasgd <zephaniahsofa@gmail.com>
2023-09-15 23:35:41 +0000

    updated main.c

--- main.c
+++ main.c
@@ -1,4 +1,52 @@
 #include "shell.h"
+/**
+ * append_path - looks for path and appends to command(TASK 3)
+ * @av: pointer array of command and arguments
+ * Return: always 0
+ */
+int append_path(char **av)
+{
+	char *command = NULL, *path = getenv("PATH"), *token, *path_copy, *full_path;
+	/* check if command already has full path or not */
+	command = av[0];
+	if (strchr(command, '/') == NULL)
+	{
+		/* get PATH value, extract directories(strtok) */
+		path_copy = strdup(path);
+		token = strtok(path_copy, ":");
+		while (token != NULL)
+		{
+			/* for each directory, generate full path */
+			/* and check if it exists (and can be accessed) */
+			full_path = malloc(strlen(token) + strlen(command) + 2);
+			if (full_path != NULL)
+			{
+				strcpy(full_path, token);
+				strcat(full_path, "/");
+				strcat(full_path, command);
+				if (access(full_path, X_OK) == 0)
+				{
+					/* file can be accessed, save full path to av[0]*/
+					/* free av[0], reallocate to size of full_path and copy */
+					free(av[0]);
+					av[0] = malloc(sizeof(char) * strlen(full_path));
+					strcpy(av[0], full_path);
+					free(full_path);
+					free(path_copy);
+					return (0);
+				}
+				free(full_path);
+			}
+			token = strtok(NULL, ":");
+		}
+		free(path_copy);
+	}
+	else
+	{
+		return (0);
+	}
+return (0);
+}
 
 /**
  * execute_command - Execute a command with its arguments.
@@ -8,8 +56,17 @@
  */
 void execute_command(char *argv, char **av, char *envp[])
 {
-	pid_t pid = fork();
+	/* argv ??? */
+	pid_t pid;
 	int status;
+	/* check if av exists or command exists(parseint worked)*/
+	if (!av || !av[0])
+	{
+		exit(EXIT_FAILURE);
+	}
+	/* handle path before fork==> call append path */
+	append_path(av);
+	pid = fork();
 
 	if (pid == -1)
 	{
