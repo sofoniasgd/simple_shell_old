3d6ad5488f3cdcd7cf65714e6d14c503b8045b3e
sofoniasgd <zephaniahsofa@gmail.com>
2023-09-18 21:39:49 +0000

    finalized strtok

--- strtok.c
+++ strtok.c
@@ -55,7 +55,7 @@ char *_strtok(char *str, const char *delim)
 			break;
 		i++;
 	}
-	printf("len: %d  i: %i\n", len, i);
+	/*printf("len: %d  i: %i\n", len, i);*/
 	/* change delimiter location to null byte '\0'*/
 	*(strc + i) = '\0';
 	/* store starting location to ret */
